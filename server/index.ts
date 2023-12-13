5d03c04be14234ee374008df19f7d1606618db07
Sheh Vincent <sheh.v123@gmail.com>
2023-12-13 14:24:36 +0800

    Added Server and TCP_Client for testing socket

---
+++ server/index.ts
@@ -0,0 +1,157 @@
+import express, { Express, Request, Response } from "express";
+import { createServer } from "http";
+import { createServer as createTcpServer } from 'net';
+import dotenv from "dotenv";
+import cors from "cors";
+
+const socketIo = require("socket.io");
+const net = require("net"); //For the raw socket TCP connections
+const regexPattern = /^(\S+)\s+(.*)$/;
+
+dotenv.config({ path: "./.env.local" }); //Make the env variable accessable
+
+interface Position {
+  lat?: number;
+  lng?: number;
+}
+
+interface ParkingInfo {
+  position?: Position;
+  currMotor?: number;
+  maxSpace?: number;
+}
+
+interface ParkingInfos {
+  [id: string]: ParkingInfo;
+}
+
+interface UpdateParkingData {
+  id: string;
+  currMotor: number;
+}
+
+class Server {
+  app: Express;
+  server: any;
+  tcpClient?: any;
+  io: any;
+  port?: any;
+  //mlServerIp?: any;
+  //mlServerPort?: any;
+  tcpServer?: any;
+  tcpPort?: any;
+  parkingInfos: ParkingInfos;
+
+  constructor() {
+    this.app = express();
+    this.server = createServer(this.app);
+    this.tcpServer = createTcpServer(this.handleTcpConnection.bind(this));
+    this.io = socketIo(this.server, {
+      cors: {
+        origin: "*",
+      },
+    });
+    this.port = process.env.PORT;
+    this.tcpPort = process.env.TCP_PORT
+    //this.mlServerIp = process.env.ML_SERVER_IP;
+    //this.mlServerPort = process.env.ML_SERVER_PORT;
+    //this.tcpClient = new net.Socket();
+
+    this.setUpRoutes();
+    this.setUpSocketIo();
+    //this.setUpTcpClient();
+    this.parkingInfos = {};
+  }
+
+  setUpRoutes() {
+    this.app.use(cors());
+    this.app.get("/", (req: Request, res: Response) => {
+      res.send("Hello World");
+    });
+  }
+
+  setUpSocketIo() {
+    this.io.on("connection", (socket: any) => {
+      socket.emit("initialization", this.parkingInfos);
+    });
+  }
+
+  startServer() {
+    this.server.listen(this.port, () => {
+      console.log(`Server listening on port ${this.port}`);
+    });
+  }
+
+  startTcpServer() {
+    this.tcpServer.listen(this.tcpPort, () => {
+      console.log(`TCP Server listening on port ${this.tcpPort}`);
+    });
+  }
+
+  handleTcpConnection(socket: any) {
+    socket.on("data", (data: any) => {
+      const receivedData = data.toString("utf8");
+      const matched = receivedData.match(regexPattern);
+      if (matched) {
+        try {
+          const command = matched[1];
+          const data = JSON.parse(matched[2]);
+          if (command === "create") {
+            this.handleCreateParkingInfo(data);
+          }
+          if (command === "update") {
+            this.handleUpdateParkingInfo(data);
+          }
+          if (command === "test") {
+            this.handleTest();
+          }
+        } catch (error) {
+          console.log(error);
+        }
+      }
+    });
+  }
+
+  handleCreateParkingInfo(data: ParkingInfos) {
+    for (const key in data) {
+      this.parkingInfos[key] = data[key];
+    }
+  }
+
+  handleUpdateParkingInfo(data: UpdateParkingData) {
+    const id = data["id"];
+    const currMotor = data["currMotor"];
+    this.parkingInfos[id]["currMotor"] = currMotor;
+    this.io.emit("update", {
+      id,
+      currMotor,
+    });
+  }
+
+  handleTest() {
+    console.log(this.parkingInfos);
+  }
+
+  /*
+  setUpTcpClient() {
+    this.tcpClient.on("data", (data: any) => {
+      console.log(data);
+    });
+    this.tcpClient.on("end", () => {
+      console.log("Connection to the ML server has ended");
+    });
+    this.tcpClient.on("error", (err: any) => {
+      console.log("TCP Client error :", err.message);
+    });
+  }
+
+  startTcpClient() {
+    this.tcpClient.connect(this.mlServerPort, this.mlServerIp, () => {
+      console.log("Connecting to ML server via TCP...");
+    });
+  }
+  */
+}
+const server = new Server();
+server.startServer();
+server.startTcpServer();
---
+++ tcp_client/tcp_client.py
@@ -0,0 +1,106 @@
+import socket
+import json
+
+def is_valid_json(msg):
+    try:
+        json.loads(msg)
+        return True
+    except:
+        return False
+
+server_host = '127.0.0.1'
+server_port = 6000
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect((server_host, server_port))
+while True:
+    json_msg = input('Input the message you want to send : \n')
+    if is_valid_json(json_msg):
+        command_msg = input('Input the command : \n')
+        msg = ' '.join([command_msg, json_msg])
+        client_socket.send(msg.encode())
+    else:
+        if json_msg == 'create_0':
+            data_dict = {
+                '000':{
+                    'position':{
+                        'lat':25.01370607644918,
+                        'lng':121.53468199176018,
+                    },
+                    'currMotor': 0,
+                    'maxSpace': 10
+                }
+            }
+            json_data = json.dumps(data_dict)
+            msg = ' '.join(['create', json_data])
+            client_socket.send(msg.encode())
+        elif json_msg == 'create_1':
+            data_dict = {
+                '001':{
+                    'position':{
+                        'lat':25.019695170584253,
+                        'lng':121.53892975940961,
+                    },
+                    'currMotor': 0,
+                    'maxSpace': 10
+                }
+            }
+            json_data = json.dumps(data_dict)
+            msg = ' '.join(['create', json_data])
+            client_socket.send(msg.encode())
+        elif json_msg == 'create_2':
+            data_dict = {
+                '002':{
+                    'position':{
+                        'lat':25.01553602325314,
+                        'lng':121.53730498100268,
+                    },
+                    'currMotor': 1,
+                    'maxSpace': 10
+                }
+            }
+            json_data = json.dumps(data_dict)
+            msg = ' '.join(['create', json_data])
+            client_socket.send(msg.encode())
+        elif json_msg == 'create_3':
+            data_dict = {
+                '003':{
+                    'position':{
+                        'lat':25.01665068859966,
+                        'lng':121.5442538922257,
+                    },
+                    'currMotor': 2,
+                    'maxSpace': 10
+                }
+            }
+            json_data = json.dumps(data_dict)
+            msg = ' '.join(['create', json_data])
+            client_socket.send(msg.encode())
+        elif json_msg == 'update_0':
+            data_dict = {
+                'id':'000',
+                'currMotor':10
+            }
+            json_data = json.dumps(data_dict)
+            msg = ' '.join(['update', json_data])
+            client_socket.send(msg.encode())
+        elif json_msg == 'update_1':
+            data_dict = {
+                'id':'001',
+                'currMotor':5
+            }
+            json_data = json.dumps(data_dict)
+            msg = ' '.join(['update', json_data])
+            client_socket.send(msg.encode())
+        elif json_msg == 'update_2':
+            data_dict = {
+                'id':'002',
+                'currMotor':10
+            }
+            json_data = json.dumps(data_dict)
+            msg = ' '.join(['update', json_data])
+            client_socket.send(msg.encode())
+        elif json_msg == 'test':
+            msg = ' '.join(['test', json.dumps({"test":"test"})])
+            client_socket.send(msg.encode())
+        else: print("INVALID INPUT")
+            
