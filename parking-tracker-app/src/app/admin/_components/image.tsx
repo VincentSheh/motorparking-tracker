aef5196bee3b7f58bef9ae0bfe62345d44007c99
theodoredave <the0dav3@gmail.com>
2023-12-16 21:10:30 +0800

    update

--- parking-tracker-app/src/app/admin/_components/image.tsx
+++ parking-tracker-app/src/app/admin/_components/image.tsx
@@ -1,13 +1,12 @@
 "use client"
-// LatestImage.tsx
 import { useEffect, useState } from 'react';
 import { getStorage, listAll, ref, getMetadata } from 'firebase/storage';
 import { initializeApp } from 'firebase/app';
 import { firebaseConfig } from '@/utils/firebase';
 
 interface LatestImageProps {
-  folderPath: string; // Path to the folder in Firebase Storage
-  alt: string; // Alt text for the image
+  folderPath: string;
+  alt: string;
 }
 
 const LatestImage: React.FC<LatestImageProps> = ({ folderPath, alt }) => {
@@ -15,30 +14,25 @@ const LatestImage: React.FC<LatestImageProps> = ({ folderPath, alt }) => {
 
   useEffect(() => {
     const initializeFirebase = async () => {
-      const app = initializeApp(firebaseConfig); // Ensure you've imported firebaseConfig
+      const app = initializeApp(firebaseConfig);
       const storage = getStorage(app);
 
       const folderRef = ref(storage, folderPath);
 
       try {
-        // List all items in the folder
         const items = await listAll(folderRef);
 
-        // Extract timestamps from file names
         const timestamps = items.items.map((item) => {
           const fileName = item.name;
           const timestamp = parseInt(fileName.split('_')[1], 10);
           return { timestamp, name: fileName };
         });
 
-        // Sort by timestamp in descending order
         timestamps.sort((a, b) => b.timestamp - a.timestamp);
 
-        // Get metadata for the latest image
         const latestImageRef = ref(storage, `${folderPath}/${timestamps[0].name}`);
         const metadata = await getMetadata(latestImageRef);
 
-        // Set the latest image URL
         setLatestImageUrl(metadata.fullPath);
       } catch (error) {
         console.error('Error fetching latest image:', error);
