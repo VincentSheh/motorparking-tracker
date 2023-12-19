40d551b269073c93d88b403760f6b2ff6b62ee9f
theodoredave <the0dav3@gmail.com>
2023-12-19 23:30:30 +0800

    almost

--- parking-tracker-app/src/components/Header.tsx
+++ parking-tracker-app/src/components/Header.tsx
@@ -9,7 +9,7 @@ export default function Header() {
 
   return (
     <div className="bg-white flex flex-col md:flex-row items-center justify-center py-3 md:py-5 top-0 z-50 relative">
-      <div className="flex flex-row items-center px-3 md:px-5 mb-2 md:mb-0 md:absolute start-0">
+      <div className="flex flex-col md:flex-row items-center px-3 md:px-5 mb-2 md:mb-0 md:absolute start-0">
         <UserButton />
         <Link className="px-2" href={isHomePage ? "/admin" : "/"}>
           {isHomePage ? "Admin" : "Home"}
@@ -18,7 +18,7 @@ export default function Header() {
       <h2 className="text-xl md:text-3xl lg:text-4xl xl:text-4xl mb-2 md:mb-0">
         PARKING TRACKER
       </h2>
-      <div className="flex flex-row items-center px-3 md:px-5 md:absolute end-0">
+      <div className="flex flex-col md:flex-row items-center px-3 md:px-5 md:absolute end-0">
         <Link className="px-2" href="/map">
           Map
         </Link>
