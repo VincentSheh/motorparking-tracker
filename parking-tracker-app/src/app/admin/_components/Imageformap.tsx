967b258044a96d2bdadc38b28222658564f9c2fb
Sheh Vincent <sheh.v123@gmail.com>
2023-12-20 08:55:39 +0800

    fixed image timestamp

--- parking-tracker-app/src/app/admin/_components/Imageformap.tsx
+++ parking-tracker-app/src/app/admin/_components/Imageformap.tsx
@@ -25,12 +25,18 @@ const Image: React.FC<LatestImageProps> = ({ folderPath, alt }) => {
           const items = await listAll(folderRef);
           const timestamps = items.items.map((item) => {
             const fileName = item.name;
-            const timestamp = parseInt(fileName.split('_')[1], 10);
+            // const timestamp = parseInt(fileName.split('_')[1], 10);
+            const parts = fileName.split(/[_\.]/); // Splits by underscore and dot
+            const datePart = parts[0]; // This should be '20231216'
+            const timePart = parts[1]; // This should be '124537'
+          
+            // Combine the date and time parts
+            const timestamp = `${datePart}_${timePart}`;
             return { timestamp, name: fileName };
           });
-  
-          timestamps.sort((a, b) => b.timestamp - a.timestamp);
-  
+          
+          timestamps.sort((a, b) => b.timestamp.localeCompare(a.timestamp));
+          console.log(timestamps)
           const latestImageRef = ref(storage, `${folderPath}/${timestamps[0].name}`);
           const downloadURL = await getDownloadURL(latestImageRef);
   
